95fe743f8bae3a645dfb349403b19656597e4b89
nealgoogs <thenextcr7@yahoo.com>
2022-03-03 19:25:32 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//Niall Geoghegan
+//CS 151
+//Chapter 2 examples
+
+public class HelloWorld{
+  
+   public static void main (String[] args){
+      
+      System.out.println("Hello world, this is Niall");
+      
+      }
+  
+}
